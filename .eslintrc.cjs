5f02d838249e1293d4c8ac36aeff72609060e137
harutas <96802323+harutas@users.noreply.github.com>
2023-08-26 18:39:35 +0900

    chore: test husky lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,10 +11,5 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  },
-  rules: {
-    // Note: you must disable the base rule as it can report incorrect errors
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error'
   }
 };
